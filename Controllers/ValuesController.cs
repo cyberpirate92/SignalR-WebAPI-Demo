c9f0074973d65901321ba81d93d7798ecd9b6117
raviteja <bsravi.tez@gmail.com>
2018-06-03 03:23:45 +0530

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+using SignalR_WebApi.Hubs;
+
+namespace SignalR_WebApi.Controllers
+{
+    [Route("api/price")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IHubContext<DataHub> dataHubContext;
+
+        public ValuesController(IHubContext<DataHub> hubContext )
+        {
+            dataHubContext = hubContext;
+        }
+
+        // POST api/price
+        [HttpPost]
+        public IActionResult Post([FromBody] double updatedPrice)
+        {
+            dataHubContext.Clients.All.SendAsync("ReceiveData", updatedPrice);
+            return Ok(true);
+        }
+    }
+}
---
+++ Hubs/DataHub.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_WebApi.Hubs
+{
+    public class DataHub : Hub
+    {
+        
+    }
+}
